219656bfb1701fc1c4082749d04151cb54787d57
Stephane Bellity <sbellity@gmail.com>
2013-07-14 11:09:42 -0600

    ...

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,20 +1,24 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  before_filter :authenticate_user
+  before_filter :current_user_id, :authenticate_user
 
   def authenticate_user
     return session.delete(:current_user) unless current_user_id
     if !session[:current_user] || session[:current_user]['id'] != current_user_id
-      @current_user = session[:current_user] = Hull.get(current_user_id)
+      @current_user = session[:current_user] = User.from_hull_user(current_user_id)
     else
       @current_user = session[:current_user]
     end
-    @github_user  = @current_user['identities'].select { |i| i['provider'] == 'github' }.first
+    if @current_user
+      @github_user  = @current_user['identities'].select { |i| i['provider'] == 'github' }.first
+    end
   end
 
   def current_user_id
     @current_user_id ||= Hull.authenticate_user(request.env)
+    Rails.logger.info "Current User: #{@current_user_id}"
+    @current_user_id
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,11 @@ class User < ActiveRecord::Base
   belongs_to :language
 
 
-  def self.from_hull_user hull_user
-    @current_user = User.find_or_create_by(:email => @hull_user['email']).first
+  def self.from_hull_user hull_user_id
+    hull_user = Hull.get(hull_user_id)
+    user = User.find_or_create_by_lastname(hull_user['name'])
+    hull_user['your_user'] = user
+    hull_user
   end
 
 end
