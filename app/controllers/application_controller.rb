bdcc0a901a872049018149d9ea5b8fb4389248ad
Stephane Bellity <sbellity@gmail.com>
2013-07-14 10:52:26 -0600

    ...

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,10 +6,11 @@ class ApplicationController < ActionController::Base
   def authenticate_user
     return session.delete(:current_user) unless current_user_id
     if !session[:current_user] || session[:current_user]['id'] != current_user_id
-      hull_user    = Hull.get(current_user_id) rescue {}
-      @current_user = session[:current_user] = User.from_hull_user(hull_user)
+      @current_user = session[:current_user] = Hull.get(current_user_id)
+    else
+      @current_user = session[:current_user]
     end
-    @github_user  ||= hull_user['identities'].select { |i| i['provider'] == 'github' }.first
+    @github_user  = @current_user['identities'].select { |i| i['provider'] == 'github' }.first
   end
 
   def current_user_id
