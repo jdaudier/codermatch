df255c00f0666c1e575f13a26692fe4d0b498fca
Stephane Bellity <sbellity@gmail.com>
2013-07-14 10:46:06 -0600

    It would be something like...

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,11 @@ class ApplicationController < ActionController::Base
 
   def authenticate_user
     return session.delete(:current_user) unless current_user_id
-    @current_user ||= (session[:current_user] = Hull.get(current_user_id))
-    @github_user  ||= @current_user['identities'].select { |i| i['provider'] == 'github' }.first
+    if !session[:current_user] || session[:current_user]['id'] != current_user_id
+      hull_user    = Hull.get(current_user_id) rescue {}
+      @current_user = session[:current_user] = User.from_hull_user(hull_user)
+    end
+    @github_user  ||= hull_user['identities'].select { |i| i['provider'] == 'github' }.first
   end
 
   def current_user_id
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,11 @@ class User < ActiveRecord::Base
 
   attr_accessible :language_id, :language, :level, :zipcode, :firstname, :lastname
   belongs_to :language
+
+
+  def self.from_hull_user hull_user
+    @current_user = User.find_or_create_by(:email => @hull_user['email']).first
+  end
+
 end
 
