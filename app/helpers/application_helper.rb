97168d7148851ba9b65718fe34a88c3894f09b3c
Joanne Daudier <jdaudier@outlook.com>
2013-07-15 09:47:54 -0500

    Don't link to styltsheet directly.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
 	def gravatar(user, size=100)
-  	default='https://dl.dropboxusercontent.com/u/125982/codermatch/stewie.gif'
+  	default = image_tag('stewie.gif')
     gravatar_id = Digest::MD5.hexdigest(user.email)
     "//gravatar.com/avatar/#{user.gravatar_id}.png?s=#{size}&d=#{default}"
   end
