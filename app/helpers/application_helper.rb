794a052ce08e511ee3b9c4c9aae94854aed7bdc4
Joanne Daudier <jdaudier@outlook.com>
2013-07-15 10:14:32 -0500

    fixed broken gravatar image

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,11 @@
 module ApplicationHelper
 	def gravatar(user, size=100)
-  	default = image_tag('stewie.gif')
+  	default = 'http://codermatch.herokuapp.com/assets/stewie.gif'
     gravatar_id = Digest::MD5.hexdigest(user.email)
-    "//gravatar.com/avatar/#{user.gravatar_id}.png?s=#{size}&d=#{default}"
+    "//gravatar.com/avatar/#{gravatar_id}.png?s=#{size}&d=#{default}"
   end
+
+  def image_url(source)
+  	URI.join(root_url, image_path(source))
+	end
 end
