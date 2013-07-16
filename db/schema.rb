320afc71f1115c994c87313d5394b9577cd790be
Joanne Daudier <jdaudier@outlook.com>
2013-07-16 09:59:41 -0500

    dropped level table

--- db/schema.rb
+++ db/schema.rb
@@ -11,20 +11,12 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130714193942) do
+ActiveRecord::Schema.define(:version => 20130716145610) do
 
   create_table "languages", :force => true do |t|
     t.string   "language"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
-  end
-
-  create_table "levels", :force => true do |t|
-    t.string   "level"
-    t.integer  "language_id"
-    t.integer  "user_id"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
   end
 
   create_table "users", :force => true do |t|
