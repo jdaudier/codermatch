d323daef3452eabb576169bcdadfc893a16e5ea8
Joanne Daudier <jdaudier@outlook.com>
2013-07-19 18:21:48 -0500

    Add config.assets.initialize_on_precompile = false to application.rb

--- config/application.rb
+++ config/application.rb
@@ -59,6 +59,7 @@ module Codermatch
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
 
-    config.assets.initialize_on_precompile = false #testing for Heroku precompilation bug
+    config.assets.initialize_on_precompile = false 
+    # Added this for Heroku precompilation bug: https://devcenter.heroku.com/articles/rails-asset-pipeline#troubleshooting
   end
 end
