1e7a9063e44c35be498929b79b25a4402c16bb4e
Joanne Daudier <jdaudier@outlook.com>
2013-07-19 18:08:47 -0500

    removed comments from codersearch controller

--- app/controllers/coder_searches_controller.rb
+++ app/controllers/coder_searches_controller.rb
@@ -4,7 +4,6 @@ class CoderSearchesController < ApplicationController
     if !@current_user
       redirect_to "/"
     else
-      # @results = User.where('languages.language' => params[:language], :level => params[:level], :zipcode => params[:zipcode]).joins(:language).where('users.id != ?', @current_user.id)
 
       if @current_user.geocoded?
         @results = @current_user.nearbys(20).where('languages.language' => params[:language], :level => params[:level]).joins(:language).where('users.id != ?', @current_user.id)
