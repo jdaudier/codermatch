9cebdca8f33a7adc3ce30068d583371ac3acf83b
Joanne Daudier <jdaudier@outlook.com>
2013-07-26 20:19:18 -0500

    User controller change from find_by_lastname to find_by_name since lastname column is deleted

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
 
   def show
     @id = params[:id]
-    @user = User.find_by_lastname(params[:id])
+    @user = User.find_by_name(params[:id])
   end
 
   
