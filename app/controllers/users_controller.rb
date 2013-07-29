d2343f0c82272cc17a87c75c247f612c2aa191b9
Joanne Daudier <jdaudier@outlook.com>
2013-07-29 17:41:11 -0500

    Add user deletion feature but does not delete Hull login and created_at and updated_at and added delete button

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 
   def show
-    @id = params[:id]
+    @id = params[:id] #@id is not a number but the GitHub login name
     @user = User.find_by_name(params[:id])
   end
 
@@ -9,6 +9,14 @@ class UsersController < ApplicationController
   	# if !params[:data][:content].empty?
   		#
   	# end
+  end
+
+  def destroy
+    # @user = User.find_by_login(params[:id])
+    @user = @current_user
+    @user.destroy
+    session[:current_user] = nil
+    redirect_to root_path
   end
 
 end
