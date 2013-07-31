a8440713673265df022c8b7d3c5ff104352711ef
Joanne <jdaudier@outlook.com>
2013-07-31 16:51:21 -0500

    Add confirmation when deleting accounts. Closes #39

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,8 +1,21 @@
 var destroyUser = function(userId){
-  $.ajax({
-    type: "POST",
-    url: "/users/" + userId,
-    dataType: "json",
-    data: {"_method":"delete"},
-  });
+
+  if (confirm('Are you sure?')) { 
+
+    $.ajax({
+      type: "POST",
+      url: "/users/" + userId,
+      dataType: "json",
+      data: {"_method":"delete"},
+    });
+
+    alert("Your account was successfully deleted. Please come back again when you feel like pair programming :)")
+
+  }  
+
+  else {
+
+    alert("Yay!! You're still with us. We are logging you out, so you can take a break :)")
+
+  };
 };
