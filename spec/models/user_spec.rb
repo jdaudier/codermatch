cd16e252e2d6bdbc1782fb9716249715834d57ff
Joanne <jdaudier@outlook.com>
2013-08-05 13:22:47 -0500

    Clean up rspec test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,10 +4,10 @@ describe User do
 
   context 'when a user chooses zipcode' do
     # This only adds user to TEST database, not our actual database. The Test DB is refreshed each time test is run so user is not permanently saved to Test DB.
-    let(:pudding) {User.create(zipcode: '78722')}
+    let(:user) {User.create(zipcode: '78722')}
     it 'adds latitude and longitude to their user object' do
-       expect(pudding.latitude).not_to eq(nil) 
-       expect(pudding.longitude).not_to eq(nil)
+       expect(user.latitude).not_to be_nil 
+       expect(user.longitude).not_to be_nil
     end
   end
 end
