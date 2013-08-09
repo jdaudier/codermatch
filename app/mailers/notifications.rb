04e49f8f5fa7e78095cee460e80f60eb5cf0eeaf
Joanne Daudier <jdaudier@outlook.com>
2013-08-09 10:42:06 -0500

    Change helper syntax in Notifications.rb

--- app/mailers/notifications.rb
+++ app/mailers/notifications.rb
@@ -1,5 +1,5 @@
 class Notifications < ActionMailer::Base
-  include ApplicationHelper
+  helper :application
   default from: "hi@codermatch.me"
 
   # Subject can be set in your I18n file at config/locales/en.yml
