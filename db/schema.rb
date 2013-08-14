a3cf927f6038ea34c3250fdcf5f9780dbe42b8ae
Joanne Daudier <jdaudier@outlook.com>
2013-08-14 09:49:39 -0500

    Updated session store.rb file to use our DB for storing session info.
    Reference article for debugging: http://stackoverflow.com/questions/9473808/cookie-overflow-in-rails-application

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130808190754) do
+ActiveRecord::Schema.define(:version => 20130814143834) do
 
   create_table "delayed_jobs", :force => true do |t|
     t.integer  "priority",   :default => 0, :null => false
@@ -34,6 +34,16 @@ ActiveRecord::Schema.define(:version => 20130808190754) do
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
+
+  create_table "sessions", :force => true do |t|
+    t.string   "session_id", :null => false
+    t.text     "data"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
+  add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
 
   create_table "users", :force => true do |t|
     t.string   "zipcode"
