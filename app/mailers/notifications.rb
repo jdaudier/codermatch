fa5a080429ec9085a63e73e288a4c9fda700c5b6
Gene Le <gene.gkl@gmail.com>
2013-09-25 22:35:07 -0500

    move modified send_notification_mailer method to notifications.rb (used to be accessed through New_developer_arbiter.rb)  Can access in console through Notifications.send_notification_mailer

--- app/mailers/notifications.rb
+++ app/mailers/notifications.rb
@@ -15,11 +15,43 @@ class Notifications < ActionMailer::Base
     mail to: recipient.email, :subject => subject
   end
 
-  def remotable_notify(recipient, new_buddy, new_buddy_language)
+  def remotable_notify(recipient, new_buddies, recipient_language)
     @recipient = recipient
-    @new_buddy = new_buddy
-    @new_buddy_language = new_buddy_language
+  
+    @new_buddies = new_buddies
+    @recipient_language = recipient_language
     subject = "There is a new coder who you might be able to pair with"
     mail to: recipient.email, :subject => subject
+  end
+
+  def send_notification_mailer
+
+    User.joins(:language).where(
+      :notify => true
+      ).each do |user|
+        @recipient = user
+        @recipient_language = @recipient.language.language
+        # The new_buddies hash will be populated with all users that meet criteria of @recipient that have registered since a week before
+        @new_buddies = []
+
+        User.joins(:language).where(
+          :language_id => @recipient.language_id,
+          :level => @recipient.level
+          ).each do |new_buddy|
+
+          @new_buddy = new_buddy
+          @new_buddy_creation = @new_buddy.created_at
+          @one_week_ago = 1.week.ago
+
+          if @new_buddy_creation >= @one_week_ago
+            @new_buddies << @new_buddy
+          end
+        end
+    
+      #TEMPORARILY removing the .delay part to test
+      Notifications.remotable_notify(@recipient, @new_buddies, @recipient_language).deliver
+
+    end
+
   end
 end
